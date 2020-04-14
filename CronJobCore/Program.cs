245d849e1ecf5726a745cfb5fe229918d7ac1da9
nsamarcos <nsamarcos@gmail.com>
2020-04-14 18:53:43 -0300

    Add files via upload

---
+++ CronJobCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CronJobCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
